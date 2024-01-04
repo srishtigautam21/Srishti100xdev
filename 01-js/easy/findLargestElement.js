5bab495a58d647eda61175645b28cc29316ff59f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-01-04 12:23:55 +0530

    0-1 Js findLargest

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let largest = numbers[0];
+  for (let num of numbers) {
+    if (num > largest) {
+      largest = num;
+    }
+  }
+  return largest;
 }
 
 module.exports = findLargestElement;
